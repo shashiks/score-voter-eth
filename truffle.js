f8d8eea6a51d18eed88dbce255779fbc110c22f1
shashiks <soni.shashikant@gmail.com>
2018-02-10 11:42:27 +0530

    ignored folders

---
+++ truffle.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const tag = require('./tags.js')
+
+const mnemonic = tag.mnemonic
+
+
+module.exports = {
+  contracts_build_directory: "./../src/contracts",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 9090,
+      network_id: "1971" // Match any network id
+    },
+	rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/4tyFJoKtfMPAzxBwkHoM", 0)
+      },
+      network_id: 4
+    }       
+  }
+};
