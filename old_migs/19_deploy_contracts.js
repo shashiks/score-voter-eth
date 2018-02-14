24a8d6da0d45a96baf6797514a4184e32c067c12
shashiks <soni.shashikant@gmail.com>
2018-02-14 17:13:18 +0530

    removed unused

--- old_migs/19_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var UserRepositoryImpl = artifacts.require("./UserRepositoryImpl.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(UserRepositoryImpl, {gas:4555000});
-};
--- old_migs/26_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var ScoreVoter = artifacts.require("./ScoreVoter.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ScoreVoter, {gas:4555000});
-};
-
